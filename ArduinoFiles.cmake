6bea1bfccf117fc4c15e97959bb220621124bcc0
Dmitriy <zapadcenter@gmail.com>
2025-09-25 00:51:00 +0300

    Добавлен файл cmake

---
+++ ArduinoFiles.cmake
@@ -0,0 +1,40 @@
+
+
+# Список общих платфоремнно-назависимых файлов проекта
+SET(ArduinoFiles
+        Arduino/abi.cpp
+        Arduino/hooks.c
+        Arduino/wiring.c
+        Arduino/IPAddress.cpp
+        Arduino/Stream.cpp
+        Arduino/wiring_digital.c
+        Arduino/CDC.cpp
+        Arduino/main.cpp
+        Arduino/Tone.cpp
+        Arduino/wiring_pulse.c
+        Arduino/new
+        Arduino/wiring_pulse.S
+        Arduino/HardwareSerial0.cpp
+        Arduino/new.cpp
+        Arduino/wiring_shift.c
+        Arduino/HardwareSerial1.cpp
+        Arduino/USBCore.cpp
+        Arduino/WMath.cpp
+        Arduino/HardwareSerial2.cpp
+        Arduino/PluggableUSB.cpp
+        Arduino/WString.cpp
+        Arduino/HardwareSerial3.cpp
+        Arduino/HardwareSerial.cpp
+        Arduino/Print.cpp
+        Arduino/WInterrupts.c
+        Arduino/wiring_analog.c
+        )
+        
+        
+        include_directories(Arduino)
+        
+        
+        
+        
+        
+
